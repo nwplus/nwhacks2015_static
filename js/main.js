0734733d6759cb8ef2cdf64b0797d3ef42306481
Robert Merki <robmerki@gmail.com>
2014-05-31 12:35:42 -0700

    good progress, first two sections complete mostly. need a bit more polish perhaps.

--- js/main.js
+++ js/main.js
@@ -1,43 +1,14 @@
-// Call Skrollr
-
-var s = skrollr.init();
-
-
-// OLD JS
-/*
-$.easing.easeInOutCubic = function (x, t, b, c, d) {
-	if ((t/=d/2) < 1) return c/2*t*t*t + b;
-	return c/2*((t-=2)*t*t + 2) + b;
-}
-
-$('header nav a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500, "easeInOutCubic");
-    return false;
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
-
-$("#faq nav a").click(function(e) {
-	e.preventDefault();
-	$("#faq article").removeAttr("class");
-	$("#faq nav a").removeAttr("class");
-	$($(this).attr("href")).addClass("active");
-	$(this).addClass("active");
-});
-
-function resize() {
-	var width = $(window).width();
-	var height = $(window).height();
-	$("section").first().css("margin-top", height + "px");
-	$("#montage a").each(function() {
-		$(this).css("width", width/$(this).siblings().andSelf().length);
-		var $img = $(this).children("img"), height = $(this).height();
-		$img.css("margin", (height - $img.height()) / 2 + "px auto");
-	});
-	$("#montage").removeAttr("class");
-}
-
-$(window).load(function(){resize();$("body").removeAttr("class")}).resize(resize);
-
-*/
-
